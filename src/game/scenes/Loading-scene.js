fb7b1ac18eb4e8b703d5b50bd3ea50bd622215cf
Anthony <e227263@edu.sbl.ch>
2025-02-06 08:29:48 +0000

    Demoprojekt weitergemacht

--- src/game/scenes/Loading-scene.js
+++ src/game/scenes/Loading-scene.js
@@ -22,6 +22,12 @@ export default class LoadingScene extends Phaser.Scene {
    * die Ressourcen dann im Game-Loop verwendet werden können.
    */
   preload() {
+    // Lade das Spritesheet für den Spieler
+    this.load.spritesheet("player", "/assets/player.png", {
+      frameWidth: 32,
+      frameHeight: 32,
+    })
+
     // Lade das Tileset für die Karten und die Objekte.
     this.load.image("tileset", "./assets/tileset.png")
 
@@ -31,6 +37,11 @@ export default class LoadingScene extends Phaser.Scene {
       "pickups",
       "./assets/tileset.png",
       "./assets/atlas/atlas-pickups.json",
+    )
+    this.load.atlas(
+      "doors",
+      "./assets/tileset.png",
+      "./assets/atlas/atlas-doors.json",
     )
 
     // Wir möchten auf das Drücken der Leertaste reagieren können, daher müssen
@@ -70,7 +81,7 @@ export default class LoadingScene extends Phaser.Scene {
       // Die Leertaste wurde gedrückt, jetzt möchten wir eine neue Szene laden.
       // Das was wir hier übergeben, ist der Schlüssel/Name der Szene, so wie
       // es im Konstruktor angegeben wurde.
-      this.scene.start("level-00")
+      this.scene.start("level-01")
     }
   }
 }
